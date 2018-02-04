d519dbcf5f0957f6087d3081bf0757629f9a6d0e
Unknown <coltonmoler@gmail.com>
2018-02-04 12:14:14 -0500

    started working on areaview

--- src/View/AreaView/TerrainView.java
+++ src/View/AreaView/TerrainView.java
@@ -1,15 +1,25 @@
 package View.AreaView;
 
+import Model.Map.Location;
 import View.Viewport;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
 public class TerrainView extends Viewport {
 
+    private Image terrainImage;
+
+    public TerrainView(String fileName, Location location){
+        ImageIcon imageIcon = new ImageIcon("fileName");
+        terrainImage = imageIcon.getImage();
+    }
+
     @Override
     public void draw(Graphics2D graphics2D) {
-
+        graphics2D.drawImage(terrainImage, parent.getX(), parent.getY(),
+                parent.getWidth(), parent.getHeight(),this );
     }
 
     @Override
