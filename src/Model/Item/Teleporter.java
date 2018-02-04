331e5d39bee00619d473c8167afee9117b2246c8
Unknown <lfreedman@ufl.edu>
2018-02-04 16:20:27 -0500

    implemented some methods in inventory

--- src/Model/Item/Teleporter.java
+++ src/Model/Item/Teleporter.java
@@ -4,7 +4,7 @@ import Model.Entity.Entity;
 import Model.Map.Location;
 import Model.Map.Map;
 
-public class Teleporter extends TakeableItem{
+public class Teleporter extends InteractiveItem{
 
     private Map map;
     private Location location;
