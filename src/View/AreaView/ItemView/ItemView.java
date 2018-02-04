a91d3d401e2d908acdcc1f9410e852b701c6f3a1
Unknown <coltonmoler@gmail.com>
2018-02-04 12:21:10 -0500

    finished AreaViewPort subsystem maybe

--- src/View/AreaView/ItemView/ItemView.java
+++ src/View/AreaView/ItemView/ItemView.java
@@ -1,7 +1,31 @@
 package View.AreaView.ItemView;
 
 
+import Model.Map.Location;
 import View.Viewport;
 
-public abstract class ItemView extends Viewport {
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class ItemView extends Viewport {
+
+    private Image itemImage;
+
+    public ItemView(String fileName, Location location){
+        ImageIcon imageIcon = new ImageIcon("fileName");
+        itemImage = imageIcon.getImage();
+    }
+
+    @Override
+    public void draw(Graphics2D graphics2D) {
+        graphics2D.drawImage(itemImage, parent.getX(), parent.getY(),
+                parent.getWidth(), parent.getHeight(),this );
+    }
+
+    @Override
+    public List<Viewport> getChildren(){
+        return null;
+    }
+
 }
