d770d752a86839f220c7d9e5b1cd4f9bb32eb92b
Colton <coltonmoler@gmail.com>
2018-02-04 15:29:26 -0500

    status view port

--- src/View/AreaView/MapView.java
+++ src/View/AreaView/MapView.java
@@ -1,5 +1,7 @@
 package View.AreaView;
 
+import Model.Entity.Entity;
+import Model.Map.Map;
 import View.Viewport;
 
 import java.awt.*;
@@ -7,6 +9,11 @@ import java.util.List;
 
 public class MapView extends Viewport {
 
+    private final int VIEW_SIZE_X = 20;
+    private final int VIEW_SIZE_Y = 20;
 
+    public MapView(Map map, Entity entity){
+
+    }
 
 }
--- src/View/StatusView/StatusViewPort.java
+++ src/View/StatusView/StatusViewPort.java
@@ -2,14 +2,42 @@ package View.StatusView;
 
 
 import Model.Entity.Entity;
+import Model.Entity.Equipment;
+import Model.Entity.Inventory;
 import View.Viewport;
 
 import java.awt.*;
 
+
 public class StatusViewPort extends Viewport {
 
-    public StatusViewPort(Entity entity){
+    public enum RenderOption{ STATS, EQUIPMENT, INVENTORY}
+
+    private RenderOption renderOption;
+
+    public StatusViewPort(Entity entity, Equipment equipment, Inventory inventory){
         add(new StatsView(entity));
+        add(new EquipmentView(equipment));
+        add(new InventoryView(inventory));
+    }
+
+    public void setRenderOption(RenderOption renderOption) {
+        this.renderOption = renderOption;
+    }
+
+    @Override
+    public void draw(Graphics2D graphics2D){
+        switch (renderOption){
+            case STATS:
+                children.get(0).draw(graphics2D);
+                break;
+
+            case EQUIPMENT:
+                children.get(1).draw(graphics2D);
+
+            case INVENTORY:
+                children.get(2).draw(graphics2D);
+        }
     }
 
 }
