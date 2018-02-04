5158b2583547846f7abe80912440e07a9dfb7ceb
Unknown <lfreedman@ufl.edu>
2018-02-04 16:54:36 -0500

    removed bad exception handling

--- src/Model/Entity/Inventory.java
+++ src/Model/Entity/Inventory.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class Inventory {
 
-//    TODO - improve generic exception handling
 //    TODO - implement observer pattern
 
     private List<TakeableItem> items;
@@ -15,45 +14,29 @@ public class Inventory {
 
     public boolean addItem(TakeableItem item){
         boolean itemAdded = false;
-        try {
-            itemAdded = items.add(item);
-        }
-        catch (Exception e) {
-            System.out.print(e);
-        }
+        itemAdded = items.add(item);
+
         return itemAdded;
     }
 
     public boolean removeItem(TakeableItem item){
         boolean itemRemoved = false;
-        try {
-            itemRemoved = items.remove(item);
-        }
-        catch (Exception e) {
-            System.out.print(e);
-        }
+        itemRemoved = items.remove(item);
+
         return itemRemoved;
     }
 
     public TakeableItem removeItem(int index){
         TakeableItem removedItem = null;
-        try {
-            removedItem = items.remove(index);
-        }
-        catch (Exception e) {
-            System.out.print(e);
-        }
+        removedItem = items.remove(index);
+
         return removedItem;
     }
 
     public boolean doesExist(TakeableItem item){
         boolean itemExists = false;
-        try{
-            itemExists = items.contains(item);
-        }
-        catch (Exception e) {
-            System.out.print(e);
-        }
+        itemExists = items.contains(item);
+
         return itemExists;
     }
 
