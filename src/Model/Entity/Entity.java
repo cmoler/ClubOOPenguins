63a17a04a691eed866e578da8192009ba0e88407
Colton <coltonmoler@gmail.com>
2018-02-05 12:13:10 -0500

    what i was thinking for movement

--- src/Model/Entity/Entity.java
+++ src/Model/Entity/Entity.java
@@ -9,6 +9,10 @@ import java.util.HashMap;
 
 public class Entity {
 
+    private int x;
+    private int y;
+    private int velocity = 5;
+
     private int health = 100; // default health?
     private int experience = 0;
     private int level = 1; // default level
@@ -61,7 +65,63 @@ public class Entity {
             return false;
     }
 
+
     public void move(Direction direction){
+        switch(direction){
+            case N:
+                y += velocity;
+                break;
+            case NE:
+                y += velocity/2;
+                x += velocity/2;
+                break;
+            case E:
+                x += velocity;
+                break;
+            case SE:
+                y -= velocity/2;
+                x += velocity;
+                break;
+            case S:
+                y -= velocity;
+                break;
+            case SW:
+                y -= velocity/2;
+                x -= velocity/2;
+                break;
+            case W:
+                x -= velocity;
+                break;
+            case NW:
+                y += velocity/2;
+                x -= velocity/2;
+                break;
+        }
+
+        int locationLeft = location.getX() * location.getWidth();
+        int locationRight = (location.getX() + 1) * location.getWidth();
+        int locationTop = location.getY() * location.getHeight();
+        int locationBottom = (location.getY() + 1) * location.getHeight();
+
+        if(locationLeft < x && x < locationRight){
+            if(y > locationTop) moveToLocation(Direction.N);
+            else if(y < locationBottom) moveToLocation(Direction.S);
+        }
+        else if(locationBottom < y && y < locationTop){
+            if(x > locationRight) moveToLocation(Direction.E);
+            else if(x < locationLeft) moveToLocation(Direction.W);
+        }
+        else if(x < locationLeft){
+            if(y > locationTop) moveToLocation(Direction.NW);
+            else if( y < locationTop) moveToLocation(Direction.SW);
+        }
+        else if(locationRight < x){
+            if(y > locationTop) moveToLocation(Direction.NE);
+            else if( y < locationTop) moveToLocation(Direction.SE);
+        }
+    }
+
+    private void moveToLocation(Direction direction){
         if(location.getAdjacentAt(direction) == null) // if trying to move off edge of map
             return;
         Location nextLocation = location.getAdjacentAt(direction);
@@ -104,11 +164,11 @@ public class Entity {
     }
 
     public int getX(){
-        return location.getX();
+        return x;
     }
 
     public int getY(){
-        return location.getY();
+        return y;
     }
 
     public int getWidth(){
