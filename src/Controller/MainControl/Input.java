14cad72068cf0e378d53969244890230deb31d64
michaelflorica <floricamichael@gmail.com>
2018-02-06 20:23:05 -0500

    handle Input

--- src/Controller/MainControl/Input.java
+++ src/Controller/MainControl/Input.java
@@ -1,12 +1,19 @@
 package Controller.MainControl;
 
-public class Input {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-    public void keyPresses(){
+public class Input implements KeyListener {
+
+    public void keyTyped(KeyEvent e) {
 
     }
 
-    public void mouseMoved(){
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
+    }
 
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Key released code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
     }
 }
--- src/Controller/MainControl/MainController.java
+++ src/Controller/MainControl/MainController.java
@@ -5,14 +5,42 @@ import Controller.GameController.GameController;
 import Controller.MainMenuControl.MainMenuController;
 import View.Viewport;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+import javax.swing.*;
+
 public class MainController {
 
+    private Input input;
     private Context context;
+    private OOPenguinGame game;
+
     private Viewport viewport;
     private GameController gameController;
     private MainMenuController mainMenuController;
 
+
+
+    public MainController(){
+        game = new OOPenguinGame();
+        input = new Input();
+        game.addKeyListener(input);
+
+
+
+    }
+
     public void setContext(Context context){
 
+    }
+
+
+
+    public static void main(String[] args){
+        MainController mainController = new MainController();
+        while(true){
+
+        }
     }
 }
