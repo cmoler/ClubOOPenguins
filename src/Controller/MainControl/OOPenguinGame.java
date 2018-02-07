c0a6d111911138a0e2a300410d4bdcdb1747d2ab
michaelflorica <floricamichael@gmail.com>
2018-02-06 20:26:05 -0500

    add OOPenguinGame

---
+++ src/Controller/MainControl/OOPenguinGame.java
@@ -0,0 +1,14 @@
+package Controller.MainControl;
+
+import javax.swing.JFrame;
+
+public class OOPenguinGame extends JFrame {
+
+    public OOPenguinGame() {
+        setTitle("OOPPenguin");
+        setSize(300, 300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
