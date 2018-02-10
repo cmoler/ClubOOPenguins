12407bc50f4feadf0b5f22fa1b303d0d2df364ad
Unknown <lfreedman@ufl.edu>
2018-02-10 15:15:44 -0500

    gamesaver tests

---
+++ tests/Controller/Utility/GameSaverTest.java
@@ -0,0 +1,20 @@
+package Controller.Utility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameSaverTest {
+    @Test
+    void saveMap() {
+    }
+
+    @Test
+    void saveEntity() {
+    }
+
+    @Test
+    void saveGame() {
+    }
+
+}
