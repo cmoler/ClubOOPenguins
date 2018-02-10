3895d7501f568c6ca43741af578f7ac04010f9e8
keanuk <keanukerr@gmail.com>
2018-02-10 10:57:13 -0500

    Implemented most of GameSaver, still needs testing

--- src/Controller/Utility/GameSaver.java
+++ src/Controller/Utility/GameSaver.java
@@ -2,33 +2,108 @@ package Controller.Utility;
 
 import Model.Entity.Entity;
 import Model.Map.World;
-import View.Viewport;
+import Model.Map.Map;
+import Model.Entity.Inventory;
+import Model.Entity.Equipment;
+import Model.Item.Item;
+import java.io.PrintWriter;
 
 public class GameSaver {
 
     private String saveFileLocation;
 
-    public void static String getTileData() {
-        String tile = " ";
+    public void SaveMap(String saveFileLocation, Entity entity, World world, Map map) {
+        String mapData = "";
 
-        //Switch statement over all tile types
+        //Placeholder until I can get actual MapID
+        mapData += "0001" + System.getProperty("line.separator");
 
-//        switch()
-    }
+        mapData += "MAP" + System.getProperty("line.separator");
+        mapData += "\t" + Integer.toString(map.getRows()) + System.getProperty("line.separator");
+        mapData += "\t" + Integer.toString(map.getCols()) + System.getProperty("line.separator");
 
-    public void SaveGame(String saveFileLocation, Entity entity, World world, Viewport viewport){
-        String worldData = "";
-        String entityData = "";
+        //Needs to be replaced with a function to return actual starting point
+        mapData += "\t" + Integer.toString(map.getRows() / 2) + "," + Integer.toString(map.getCols() / 2) + System.getProperty("line.separator");
 
-        //A getTile function in the viewport or the model could let us iterate over all the map tiles to save the world map
-        //Not sure this is the best solution, but should work for the map
+        mapData += "\tLOCATIONS";
 
-        for(int i = 0; i < viewport.getX(); i++) {
-            for (int j = 0; i < viewport.getY(); j++) {
-                worldData += getTileData(viewport.getTile(i, j));
+        for(int i = 0; i < map.getRows(); i++) {
+            for(int j = 0; j < map.getCols(); j++) {
+                mapData += "\t\t" + Integer.toString(j) + "," + Integer.toString(i) + System.getProperty("line.separator");
+                mapData += "\t\t\t" + map.getLocation(j, i).getTerrain().toString().toUpperCase();
+
+                if(map.getLocation(j, i).moveAllowed(entity)) mapData += "\t\t\tFALSE" + System.getProperty("line.separator");
+                else mapData += "\t\t\tTRUE" + System.getProperty("line.separator");
+
+                mapData += "\t\t\t" + map.getLocation(j, i).getAreaEffect().toString().toUpperCase() + System.getProperty("line.separator");
+                mapData += "\t\t\tITEMS" + System.getProperty("line.separator");
+
+                //Converts Item list to string array
+                String[] items = new String[map.getLocation(j, i).getItems().size()];
+                for(Object value : map.getLocation(j, i).getItems()) {
+                    mapData += "\t\t\t\t" + value.toString() + System.getProperty("line.separator");
+                    if(value.toString().equals("TELEPORTER")) {
+                        //Get teleporter info
+                    }
+                    else {
+                        //Get item details
+                    }
+                }
             }
         }
 
-        //Entity could be saved to a JSON file
+        try(PrintWriter mapSave = new PrintWriter(saveFileLocation + "Map_Save.txt")) {
+            mapSave.println(mapData);
+            mapSave.close();
+        }
+        catch (Exception e) {
+            System.out.println("Exception when writing to file");
+        }
+    }
+
+    public void SaveEntity(String saveFileLocation, Entity entity, World world, Map map, Inventory inventory, Equipment equipment) {
+        String entityData = "";
+
+        //Placeholder until I can get actual EntityID
+        entityData += "0001" + System.getProperty("line.separator");
+
+        entityData += "ENTITY" + System.getProperty("line.separator");
+
+        entityData += "\t" + entity.getEntityType().toString().toUpperCase() + System.getProperty("line.separator");
+
+        entityData += "\t" + entity.getLocation().toString();
+
+        entityData += "\t" + Integer.toString(entity.getHealth()) + System.getProperty("line.separator");
+
+        entityData += "\t" + Integer.toString(entity.getExperience()) + System.getProperty("line.separator");
+
+        entityData += "\t" + "INVENTORY" + System.getProperty("line.separator");
+        Inventory.InventoryIterator invIter = inventory.getIterator();
+        invIter.reset();
+
+        while(invIter.hasNext()) {
+            entityData += "\t\t" + invIter.getCurrent().toString() + System.getProperty("line.separator");
+            invIter.next();
+        }
+
+        entityData += "\t\t" + "EQUIPMENT" + System.getProperty("line.separator");
+        entityData += "\t\t\t" + equipment.getEquipped().toString() + System.getProperty("line.separator");
+
+        try(PrintWriter entitySave = new PrintWriter(saveFileLocation + "Entity_Save.txt")) {
+            entitySave.println(entityData);
+            entitySave.close();
+        }
+        catch (Exception e) {
+            System.out.println("Exception when writing to file");
+        }
+
+    }
+
+    public void SaveGame(String saveFileLocation, Entity entity, World world, Map map, Inventory inventory, Equipment equipment){
+
+        SaveMap(saveFileLocation, entity, world, map);
+
+        SaveEntity(saveFileLocation, entity, world, map, inventory, equipment);
+
     }
 }
