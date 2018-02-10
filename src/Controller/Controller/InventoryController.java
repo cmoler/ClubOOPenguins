ed3cb8c7d85f4ee09eff636c9ab93cb08f35bade
Unknown <zeineddine.jad@gmail.com>
2018-02-10 18:13:06 -0500

    Added required game instantiations to controllers

--- src/Controller/Controller/InventoryController.java
+++ src/Controller/Controller/InventoryController.java
@@ -1,11 +1,14 @@
 package Controller.Controller;
 
+import Configs.InventorySizes;
 import Controller.Contexts.GameContext;
 import Model.Map.Direction;
+import View.StatusView.StatusViewPort;
 
 public class InventoryController implements Controller {
 
     private MainController mainController;
+    private int itemIndex;
 
     public InventoryController(MainController mainController) {
         this.mainController = mainController;
@@ -16,18 +19,45 @@ public class InventoryController implements Controller {
         mainController.setActiveContext(GameContext.MENU);
     }
 
+
     @Override
     public void handleI() {
+        mainController.getViewPort().setRenderOption(StatusViewPort.RenderOption.INVENTORY);
 
     }
 
     @Override
     public void handleE() {
-
+        mainController.getViewPort().setRenderOption(StatusViewPort.RenderOption.EQUIPMENT);
     }
 
     @Override
     public void handleNumpad(Direction direction) {
-
+        switch(direction){
+            case N:
+                itemIndex -= InventorySizes.INVENTORY_COLUMNS;
+                if(itemIndex < 0){
+                    itemIndex = 0;
+                }
+                break;
+            case W:
+                itemIndex += 1;
+                if(itemIndex > (InventorySizes.INVENTORY_COLUMNS  * InventorySizes.INVENTORY_ROWS)){
+                    itemIndex = (InventorySizes.INVENTORY_COLUMNS  * InventorySizes.INVENTORY_ROWS);
+                }
+                break;
+            case E:
+                itemIndex -= 1;
+                if(itemIndex < 0){
+                    itemIndex = 0;
+                }
+                break;
+            case S:
+                itemIndex += InventorySizes.INVENTORY_COLUMNS;
+                if(itemIndex > (InventorySizes.INVENTORY_COLUMNS  * InventorySizes.INVENTORY_ROWS)){
+                    itemIndex = (InventorySizes.INVENTORY_COLUMNS  * InventorySizes.INVENTORY_ROWS);
+                }
+                break;
+        }
     }
 }
--- src/Controller/Controller/MenuController.java
+++ src/Controller/Controller/MenuController.java
@@ -1,35 +1,78 @@
 package Controller.Controller;
 
+import Controller.Utility.EntityBuilder;
 import Controller.Utility.GameLoader;
 import Controller.Utility.GameSaver;
+import Controller.Utility.MapBuilder;
 import Model.Entity.Entity;
 import Controller.Contexts.GameContext;
 import Model.Map.Direction;
+import View.AreaView.AvatarView;
+import View.AreaView.MapView;
+import View.StatusView.StatusViewPort;
+import View.Viewport;
+
+import java.io.FileNotFoundException;
 
 public class MenuController implements Controller {
 
     private MainController mainController;
     private GameLoader gameLoader;
     private GameSaver gameSaver;
+    private MapBuilder mapBuilder;
+    private EntityBuilder entityBuilder;
+
+
 
     public MenuController(MainController mainController){
         this.mainController = mainController;
+
+        mapBuilder = new MapBuilder();
+        try {
+            mapBuilder.buildMap("0001");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        MapView mapView = mapBuilder.getViewport();
+
+        entityBuilder = new EntityBuilder();
+        try {
+            entityBuilder.buildEntity("0001");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        AvatarView avatarView = entityBuilder.getAvatarView();
+
+        StatusViewPort statusViewPort = entityBuilder.getStatusViewport();
+        statusViewPort.setRenderOption(StatusViewPort.RenderOption.INVENTORY);
+
+        mapView.setEntity(avatarView.getEntity());
+
+        Viewport viewport = new Viewport();
+        viewport.add(mapView);
+        viewport.add(avatarView);
+        viewport.add(statusViewPort);
+
+        mainController.setViewPort(viewport);
     }
 
     @Override
     public void handleEscape() {
         System.out.println("Command: close menu");
         mainController.setActiveContext(GameContext.AREA);
+
     }
 
     @Override
     public void handleI() {
+        mainController.getViewPort().setRenderOption(StatusViewPort.RenderOption.INVENTORY);
 
     }
 
     @Override
     public void handleE() {
-
+        mainController.getViewPort().setRenderOption(StatusViewPort.RenderOption.EQUIPMENT);
     }
 
     public void handleNumpad(Direction direction) {
@@ -44,7 +87,9 @@ public class MenuController implements Controller {
 
     }
 
-    public void newGame(){
+    public void startGame(){
+        String def = "";
+
 
     }
 
