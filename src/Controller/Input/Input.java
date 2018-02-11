28a084731b7dba52e2a8207632aaccafa2cbc525
Colton <coltonmoler@gmail.com>
2018-02-11 10:23:38 -0500

    added numberinput

--- src/Controller/Input/Input.java
+++ src/Controller/Input/Input.java
@@ -28,35 +28,51 @@ public class Input implements KeyListener {
             case KeyEvent.VK_NUMPAD1:
                 activeContext.processNumpadEvent(Direction.SW);
                 break;
-
+            case KeyEvent.VK_1:
+                activeContext.processNumpadEvent(Direction.SW);
+                break;
             case KeyEvent.VK_NUMPAD2:
                 activeContext.processNumpadEvent(Direction.S);
                 break;
-
+            case KeyEvent.VK_2:
+                activeContext.processNumpadEvent(Direction.S);
+                break;
             case KeyEvent.VK_NUMPAD3:
                 activeContext.processNumpadEvent(Direction.SE);
                 break;
-
+            case KeyEvent.VK_3:
+                activeContext.processNumpadEvent(Direction.SE);
+                break;
             case KeyEvent.VK_NUMPAD4:
                 activeContext.processNumpadEvent(Direction.W);
                 break;
-
+            case KeyEvent.VK_4:
+                activeContext.processNumpadEvent(Direction.W);
+                break;
             case KeyEvent.VK_NUMPAD6:
                 activeContext.processNumpadEvent(Direction.E);
                 break;
-
+            case KeyEvent.VK_6:
+                activeContext.processNumpadEvent(Direction.E);
+                break;
             case KeyEvent.VK_NUMPAD7:
                 activeContext.processNumpadEvent(Direction.NW);
                 break;
-
+            case KeyEvent.VK_7:
+                activeContext.processNumpadEvent(Direction.NW);
+                break;
             case KeyEvent.VK_NUMPAD8:
                 activeContext.processNumpadEvent(Direction.N);
                 break;
-
+            case KeyEvent.VK_8:
+                activeContext.processNumpadEvent(Direction.N);
+                break;
             case KeyEvent.VK_NUMPAD9:
                 activeContext.processNumpadEvent(Direction.NE);
                 break;
-
+            case KeyEvent.VK_9:
+                activeContext.processNumpadEvent(Direction.NE);
+                break;
             default:
                 return;
 
@@ -71,7 +87,9 @@ public class Input implements KeyListener {
         if(keyCode >= KeyEvent.VK_NUMPAD1 && keyCode <= KeyEvent.VK_NUMPAD9){
             forwardNumPadEvent(event);
         }
-
+        if(keyCode >= KeyEvent.VK_1 && keyCode <= KeyEvent.VK_9){
+            forwardNumPadEvent(event);
+        }
         else{
             forwardKeyEvent(event);
         }
