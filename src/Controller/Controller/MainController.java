fe70012da750c278b807f71e011178e1b8ea37f1
Unknown <zeineddine.jad@gmail.com>
2018-02-10 21:49:05 -0500

    Merge

--- src/Controller/Controller/MainController.java
+++ src/Controller/Controller/MainController.java
@@ -100,6 +100,32 @@ public class MainController {
 
     public void gameLoop() {
 
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames + " TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
     }
 
     public void setMenuRender() {
