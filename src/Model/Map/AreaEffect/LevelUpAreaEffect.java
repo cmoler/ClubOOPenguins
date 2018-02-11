baf8eef1e05d995b0420670aefec91ac983b6b21
Mac Macoy <mmacoy@ufl.edu>
2018-02-11 14:22:20 -0500

    level up area affect

--- src/Model/Map/AreaEffect/LevelUpAreaEffect.java
+++ src/Model/Map/AreaEffect/LevelUpAreaEffect.java
@@ -7,7 +7,7 @@ public class LevelUpAreaEffect extends AreaEffect{
     public LevelUpAreaEffect(){}
 
     public void affect(Entity entity) {
-        // make entity level up
-        // TODO: need to know if level up is automatic or not
+        int experienceToAdd = entity.getExperienceForNextLevel() - entity.getExperience();
+        entity.gainExperience(experienceToAdd);
     }
 }
